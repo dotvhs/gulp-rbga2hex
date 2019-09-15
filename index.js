bd16b18cd08ee7a6e4a170b5431ab653d0c829fb
dotvhs <voythas@gmail.com>
2019-09-15 17:55:21 +0200

    Fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var PluginError = gutil.PluginError;
 var PLUGIN_NAME = "gulp-rgba2hex";
 
 function rgbaToHex(rgba) {
-	var color = rgba.splt(",");
+	var color = rgba.split(",");
 	return color && color.length === 4
 		? "#" +
 				("0" + parseInt(parseFloat(color[3]) * 255, 10).toString(16)).slice(
